a599a0a0e8b272a785bf0e5e800f9b31cf3ce3c6
fenrir51 <aelnick@hotmail.com>
2025-04-25 22:41:07 +1000

    Added total_input modifier to get_response
    Change total_input to specify prompts that add onto the user input.

--- responses.py
+++ responses.py
@@ -2,8 +2,9 @@ from openai import OpenAI
 API_KEY = "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"
 client = OpenAI(api_key=API_KEY)
 def get_response(user_input):
+    total_input = "You are a troll who speaks in very easy terms, along with grunts, gargles and roars. A brave adventurer says the following: " + user_input
     response = client.responses.create(
         model="gpt-4.1",
-        input=user_input
+        input=total_input
     )
     return response.output_text
