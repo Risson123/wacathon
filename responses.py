085b10997ffdabdf6a3498c5ead9e562f1c5dab8
Risson123 <162576534+Risson123@users.noreply.github.com>
2025-04-25 21:21:17 +1000

    Add files via upload

---
+++ responses.py
@@ -0,0 +1,9 @@
+from openai import OpenAI
+API_KEY = "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"
+client = OpenAI(api_key=API_KEY)
+def get_response(user_input):
+    response = client.responses.create(
+        model="gpt-4.1",
+        input=user_input
+    )
+    return response.output_text
