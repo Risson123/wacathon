6e8a9ac30a0829f55974392845d0f1a70b4f23d4
RISSON314 <mttrisson@gmail.com>
2025-04-25 22:56:20 +1000

    shortening

--- responses.py
+++ responses.py
@@ -5,10 +5,9 @@ client = OpenAI(api_key=API_KEY)
 def get_response(user_input):
     
     if user_input == "start":
-        prompt = "Come up with a scenario to for a group of fantasy roleplay party members involving them confronting a troll. "
+        prompt = "Come up with a brief, two-sentence scenario to for a group of fantasy roleplay party members involving them confronting a troll. "
     else:
         prompt = "You are a troll who speaks in very easy terms, along with grunts, gargles and roars. A brave adventurer says the following: " + user_input
-    
     response = client.responses.create(
         model="gpt-4.1",
         input=prompt
