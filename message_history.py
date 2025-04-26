81c50b2e21906645db8b26aa57df4c8e9d592443
RISSON314 <mttrisson@gmail.com>
2025-04-26 14:58:35 +1000

    New message history logic

--- message_history.py
+++ message_history.py
@@ -1,15 +1,23 @@
+from discord import Message
 class MessageHistory:
     def __init__(self):
         self.clear_history()
-    
+        self.prompt = ""
+        self.message_history: list[Message] = []
     def add_prompt(self, gpt_prompt:str) -> None:
+    # SETS THE PROMPT FOR THE CURRENT MESSAGE HISTORY
         self.prompt = gpt_prompt
 
-    def add_response(self, user_response:str ) -> None:
-        self.message_history.append(user_response)
+    def add_response(self, user_response:Message ) -> None:
+    # ADDS THE RESPONSE TO SELF.MESSAGE_HISTORY
+        self.message_history.append(user_response.content)
 
     def return_message_history(self) -> str:
-        return "\n".join(self.message_history)
+        ret = ""
+        for message in self.message_history:
+            ret += f"{message.author}'s response was to {message.content.replace("!response", "")}"
+        return ret
+            
 
     def clear_history(self):
         self.message_history = []
