7b6da94b6bd7900af48b4af88ddc228b673da958
fenrir51 <aelnick@hotmail.com>
2025-04-26 15:01:51 +1000

    Added extra spacing in responses.py

--- responses.py
+++ responses.py
@@ -13,8 +13,10 @@ def get_response(user_input, messageHistory: MessageHistory):
         )
         messageHistory.add_prompt(response.output_text)
         return response.output_text
+    
     elif user_input.startswith("response"):
         return messageHistory.return_message_history()
+    
     elif user_input == "end" and messageHistory.prompt != None:
         prompt = "You are in the following scenario: " + messageHistory.prompt + "\nCreate an outcome within 1000 characters if the players take the following actions: " + messageHistory.return_message_history()
         messageHistory.clear_history()
@@ -23,4 +25,5 @@ def get_response(user_input, messageHistory: MessageHistory):
             input=prompt
         )
         return response.output_text
+    
     return
